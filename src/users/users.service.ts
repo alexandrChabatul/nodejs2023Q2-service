f78f7ee7de95ace4dcf65e1e623303d95fa7d976
sashkill94 <sashkill94@gmail.com>
2023-07-29 16:10:57 +0300

    feat: add services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './types/user.interface';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+
+const users: User[] = [];
 
 @Injectable()
 export class UsersService {
+
+  saltOrRounds = 10;
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = {
+      id: uuidv4(); // uuid v4
+      login: createUserDto.login;
+      password: bcrypt.ha;
+      version: number; // integer number, increments on update
+      createdAt: number; // timestamp of creation
+      updatedAt: number; // timestamp of last update
+    }
   }
 
   findAll() {
