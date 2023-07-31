b511c4b770053eb6b069457bd0cb862128d4942e
sashkill94 <sashkill94@gmail.com>
2023-07-31 23:08:23 +0300

    feat: add docs

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const port = config.get('PORT') || 3000;
+  const apiSpecPath = join(__dirname, '../doc/api.yaml');
+  const apiSpec = yaml.load(readFileSync(apiSpecPath, 'utf8'));
+  SwaggerModule.setup('api', app, apiSpec);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
