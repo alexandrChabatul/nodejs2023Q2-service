e4447c21c161c5d2cdbd465cb97e4577561f5557
sashkill94 <sashkill94@gmail.com>
2023-08-08 18:39:51 +0300

    feat: add artist entity

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -19,31 +19,31 @@ export class ArtistsController {
 
   @Post()
   @HttpCode(201)
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistsService.create(createArtistDto);
+  async create(@Body() createArtistDto: CreateArtistDto) {
+    return await this.artistsService.create(createArtistDto);
   }
 
   @Get()
-  findAll() {
-    return this.artistsService.findAll();
+  async findAll() {
+    return await this.artistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.artistsService.findOne(id);
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.artistsService.findOne(id);
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateArtistDto: UpdateArtistDto,
   ) {
-    return this.artistsService.update(id, updateArtistDto);
+    return await this.artistsService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.artistsService.remove(id);
+  async remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.artistsService.remove(id);
   }
 }
--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,13 +1,11 @@
-import { v4 as uuidv4 } from 'uuid';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('artists')
 export class Artist {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column()
   name: string;
+  @Column()
   grammy: boolean;
-
-  constructor(name: string, grammy: boolean) {
-    this.id = uuidv4();
-    this.name = name;
-    this.grammy = grammy;
-  }
 }
